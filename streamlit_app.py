c259e7be84397788fbc219271491f3bbb2bd6672
VikashKumarJhaST <151021584+VikashKumarJhaST@users.noreply.github.com>
2023-12-29 22:35:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit
-streamlit.title("hello world");
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")\
+streamlit.header("My Dataframe");
+streamlit.dataframe(my_fruit_list)
