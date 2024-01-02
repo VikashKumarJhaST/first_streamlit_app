c5cd55f66d2dcf66391a53aadbb55c057a2b364e
VikashKumarJhaST <151021584+VikashKumarJhaST@users.noreply.github.com>
2024-01-02 19:26:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ streamlit.dataframe(fruits_to_show )
 
 #display fruitvice api
 streamlit.header("fruityvice fruit advice");
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+kiwi)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 streamlit.text(fruityvice_response.json())
 # normalize the data 
 # display normal data(in tables)
